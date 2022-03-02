32224e313b8dea5adcb985a33da89a96b2a0691e
fefe <fedescalada17@hotmail.com>
2022-03-01 22:10:10 -0300

    arreglo de titulo

--- src/pages/welcome/index.ts
+++ src/pages/welcome/index.ts
@@ -40,7 +40,7 @@ export function initWelcomePage (params){
             justify-content: center;
             padding:40px;
             max-width:317px;
-            margin-top: 10px;
+            margin-top: -50px;
             color: #FAFAFA
 
         }
